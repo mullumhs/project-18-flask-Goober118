a24f483531e662345b5898bdce271773eeacd48c
Torin Forsyth <torin.forsyth@DETNSW>
2024-11-01 10:39:01 +1100

    Did the devils tango

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
+
+
+
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'Goodbye Cruel World'
+
+
+@app.route('/inventory')
+
+def inventory():
+
+    inventory_items = ['apple', 'banana', 'cherry']
+
+    return render_template('other_templates.html', inventory=inventory_items)
+
+
 
 if __name__ == '__main__':
+
     app.run(debug=True)
